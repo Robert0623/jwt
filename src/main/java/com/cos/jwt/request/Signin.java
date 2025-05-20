61fbf7c03b37239f56753b48e806727aacb9c287
hwoo <tone8943@gmail.com>
2025-05-21 00:35:03 +0900

    누락분 추가

---
+++ src/main/java/com/cos/jwt/request/Signin.java
@@ -0,0 +1,13 @@
+package com.cos.jwt.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Signin {
+
+    private String username;
+    private String password;
+
+}
